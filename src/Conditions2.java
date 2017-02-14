97ce7509673229029f1ea213eb551b642da29823
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-02-14 09:26:33 -0500

    conditions 2-corrected

--- src/Conditions2.java
+++ src/Conditions2.java
@@ -19,59 +19,69 @@ public class Conditions2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // create new city
+         // create a city
         City kw = new City();
         
+        // create a robot
+        Robot karel = new Robot(kw, 4, 1, Direction.EAST);
         
-        //create a robot
-        Robot karel= new Robot(kw,4,1,Direction.EAST);
+        // create the path
+        new Thing(kw, 4, 2);
         
+        new Thing(kw, 4, 3);
+        new Thing(kw, 4, 3);
         
-        //create path to follow
-        new Thing (kw,4,2);
-        new Thing (kw,4,3);
-        new Thing (kw,4,3);
+        new Thing(kw, 3, 3);
         
-        new Thing (kw,3,3);
+        new Thing(kw, 2, 3);
+        new Thing(kw, 2, 3);
+        new Thing(kw, 2, 3);
+        new Thing(kw, 2, 3);
         
-        new Thing (kw,2,3);
-        new Thing (kw,2,3);
-        new Thing (kw,2,3);
-        new Thing (kw,2,3);
-        
-        //Show number of things count
+        // turn on the thing labels
         kw.showThingCounts(true);
         
-        
-        //step onto the first part of path
+        // step onto the first part of path
         karel.move();
         
-        //loop
+        // loop
         while(true){
-            //if there are things 
-            //      pick them all up 
-                   while(karel.canPickThing()){
-                    karel.pickThing();
-            
+            // if there are things
+            //      pick them all up
+            while(karel.canPickThing()){
+                karel.pickThing();
+            }
             // if holding 1 thing, move
-            if(karel.countThingsInBackpack()== 1)
-                //drop things
+            if(karel.countThingsInBackpack() == 1){
+                // drop things
                 karel.putThing();
-                //move
+                // move
                 karel.move();
-             
             // if holding 2 things, turn left
-             } if(karel.countThingsInBackpack()== 2){ 
-                //drop things
+            }else if(karel.countThingsInBackpack() == 2){
+                // drop things
                 karel.putThing();
                 karel.putThing();
-                //move
+                // move
                 karel.turnLeft();
                 karel.move();
-            // if holding 3 things, turn right
-                if(karel.countThingsInBackpack())
-            // if holding 4 things, stop
-             }
+            }else if(karel.countThingsInBackpack() == 3){
+                // drop things
+                karel.putThing();
+                karel.putThing();
+                karel.putThing();
+                // move
+                karel.turnLeft();
+                karel.turnLeft();
+                karel.turnLeft();
+                karel.move();
             }
-}
+            
+            // if holding 3 things, turn right
+            // if holding 4 things, stop
+        }
+        
+        
+    }
+    
 }
