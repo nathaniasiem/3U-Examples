0591f97865e484d4ddf500159d519aa7de3501de
candy <candy@192.168.0.15>
2017-03-11 10:33:22 -0500

    Conditions example with format corrected

--- src/Conditions.java
+++ src/Conditions.java
@@ -9,7 +9,6 @@ import becker.robots.Wall;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author simon7323
@@ -20,11 +19,11 @@ public class Conditions {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         // Create a city
+        // Create a city
         City kw = new City();
         // Create a Robot
         Robot karel = new Robot(kw, 1, 1, Direction.EAST);
-        
+
         // put up a wall
         new Wall(kw, 1, 5, Direction.EAST);
         new Thing(kw, 0, 1);
@@ -36,26 +35,24 @@ public class Conditions {
         new Thing(kw, 0, 1);
         new Thing(kw, 0, 1);
         new Thing(kw, 0, 1);
-        
-       new Thing(kw, 0, 1);
-        
-       // ask the robot if the front is clear
-        while(karel.frontIsClear()){
+
+        new Thing(kw, 0, 1);
+
+        // ask the robot if the front is clear
+        while (karel.frontIsClear()) {
             // move and turn left
             karel.move();
             karel.turnLeft();
             // if there is something to pick up
-            if(karel.canPickThing()){
+            if (karel.canPickThing()) {
                 // pick up everything
                 karel.pickThing();
                 break;
             }
-        //did karel hit a wall
-        if(!(karel.frontIsClear() )){
-            karel.turnLeft();
+            //did karel hit a wall
+            if (!(karel.frontIsClear())) {
+                karel.turnLeft();
+            }
         }
-        }
-        
-        
-    
-
+    }
+}
